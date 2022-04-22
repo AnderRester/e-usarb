b158a0dd564c11992df632c2c550069d0adc8c9f
anderrester <anderrester@mail.ru>
2022-04-22 08:24:21 +0300

    structure

---
+++ e-usarb/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header'
+
+export default function App() {
+  return (
+    <View>
+        <View>
+        <Header />
+      </View>
+      <View>
+
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ e-usarb/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Image, Linking, TouchableOpacity, Text } from "react-native";
+
+const Header = () => {
+    return <View style={{ backgroundColor: 'grey', paddingTop: 55, alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row', paddingBottom: 10, textAlign: 'center'}}>
+        <View style={{ paddingLeft: 18 }}>
+            <TouchableOpacity onPress={() => Linking.openURL('http://usarb.md')}>
+                <Image source={{ uri: 'http://www.lnss-projects.pendor.de/img/logos/USARB.png' }} style={{
+                    width: 70,
+                    height: 55,
+                }} />
+            </TouchableOpacity>
+        </View>
+        <View><Text style={{fontSize: 24, fontWeight: 600}}>USARB</Text></View>
+        <View style={{ paddingRight: 24, borderColor: 'red' }}>
+            <TouchableOpacity onPress={() => Linking.openURL('http://usarb.md')}>
+                <Image source={require('../../personalAsset/chat/send.svg')} style={{
+                    width: 30,
+                    height: 30,
+                }} />
+            </TouchableOpacity>
+        </View>
+    </View>
+}
+
+export default Header
